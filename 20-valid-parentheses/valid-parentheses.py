2e826a19454404c89aa700326b3de92774089cb7
Priya Gupta <guptapriya83900@gmail.com>
2025-03-11 23:12:19 -0700

    Time: 1 ms (32.71%) | Memory: 18 MB (10.37%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic={ ")" : "(", "]" : "[", "}" : "{" }
+        stack=[]
+        
+        if len(s)%2!=0:
+            return False
+        
+
+        for i in range(len(s)):
+            if s[i] in dic:
+                if stack and dic[s[i]]==stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(s[i])
+
+        return True if not stack else False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        
